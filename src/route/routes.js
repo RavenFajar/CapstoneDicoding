a0e24485eaa9687dca328d730fa02c6d5507927c
RavenFajar <102020195+RavenFajar@users.noreply.github.com>
2024-06-04 18:25:57 +0700

    Update routes.js

--- src/route/routes.js
+++ src/route/routes.js
@@ -10,29 +10,35 @@ const routes = {
 
 // Fungsi untuk memuat script yang sesuai dengan halaman
 function loadScript(scriptName) {
-  const scriptId = `script-${scriptName}`;
-  if (!document.getElementById(scriptId)) {
-    const script = document.createElement('script');
-    script.id = scriptId;
-    script.src = `../view/${scriptName}.js`;
-    document.body.appendChild(script);
+  const scriptId = `${scriptName}`;
+  // Hapus script lama jika sudah ada
+  const existingScript = document.getElementById(scriptId);
+  if (existingScript) {
+    document.body.removeChild(existingScript);
   }
+
+  // Buat dan tambahkan script baru
+  const script = document.createElement('script');
+  script.id = scriptId;
+  script.src = `../view/${scriptName}.js`;
+  document.body.appendChild(script);
 }
+
 
 // Fungsi untuk mengatur konten main berdasarkan route
 function loadContent(route) {
   const mainContent = document.getElementById('dynamicContent');
-  mainContent.innerHTML = ''; // Kosongkan konten sebelumnya
   const scriptName = routes[route] || 'notfound';
   loadScript(scriptName);
 }
+
 
 // Fungsi untuk mengelola navigasi
 function handleNavigation(event) {
   event.preventDefault();
   const href = event.target.getAttribute('href');
-  const route = href.startsWith('#') ? href.slice(1) : '/';
-  window.history.pushState({}, '', `#${route}`);
+  const route = href.slice(1); // Langsung gunakan href karena sudah sesuai dengan route yang diinginkan
+  window.history.pushState({}, '', `${route}`); // Tambahkan # untuk konsistensi pada URL
   loadContent(route);
 }
 
@@ -44,7 +50,8 @@ function initRouter() {
   });
 
   // Muat konten awal berdasarkan URL
-  const initialRoute = window.location.hash.slice(1) || '/';
+  const initialRoute = window.location.slice(1) || '/';
+  console.log(initialRoute);
   loadContent(initialRoute);
 
   // Atur event listener untuk mengelola navigasi kembali
@@ -54,5 +61,4 @@ function initRouter() {
   });
 }
 
-// Panggil fungsi initRouter saat halaman dimuat
-document.addEventListener('DOMContentLoaded', initRouter);
+initRouter();
